89be739c7cf4c919deebf46416b94c38a0f96ac7
645996795@qq.com <645996795@qq.com>
2019-02-27 20:32:24 +0800

    added files

--- openlog/openlog.c
+++ openlog/openlog.c
@@ -3,7 +3,7 @@
 
 int main(int argc, char *argv[])
 {
-	openlog("test.txt", LOG_CONS | LOG_PID, LOG_USER);
+	openlog("test_log", LOG_CONS | LOG_PID, LOG_USER);
 	syslog(LOG_INFO, "This is a massage just for test\n");
 	closelog();
 	return 0;
