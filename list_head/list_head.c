0ca0c9abb04fd4b94407d589fe0d05f0faab093e
645996795@qq.com <645996795@qq.com>
2019-03-01 15:17:50 +0800

    added files

--- list_head/list_head.c
+++ list_head/list_head.c
@@ -8,30 +8,24 @@ int main(int argc, char* argv[])
 	int age_i, weight_j;
 	char sex_k;
 
-	/* 定义并初始化一个链表头 */
 	person_t person_head;
+
 	INIT_LIST_HEAD(&person_head.list);
 	
 	for(age_i = 10, weight_j = 35; age_i < 40; age_i += 5, weight_j += 5){
 		tmp =(person_t*)malloc(sizeof(person_t));
 		tmp->age = age_i;
 		tmp->weight = weight_j;
-		/* 把这个节点链接到链表后面 
-		 * 这里因为每次的节点都是加在person_head的后面，所以先加进来的节点就在链表里的最后面
-		 * 打印的时候看到的顺序就是先加进来的就在最后面打印
-		 */ 
 		list_add(&(tmp->list), &(person_head.list)); 
 	}
 
-	/* 下面把这个链表中各个节点的值打印出来*/
-	printf("\n");
 	printf("print the list\n");
+
 	list_for_each(pos, &person_head.list){
-		tmp = list_entry(pos, person_t, list); /* 取得pos所在结构指针 */
+		tmp = list_entry(pos, person_t, list); 
 		printf("age:%d,  weight: %d, sex: %s \n", tmp->age, tmp->weight, tmp->sex);
 	}
-	printf("\n");
-	/* 删除节点，age20 */
+
 	printf("print list after delete a node which age is 20\n");
 	list_for_each_safe(pos, n, &person_head.list){
 		tmp = list_entry(pos, person_t, list);
@@ -51,5 +45,5 @@ int main(int argc, char* argv[])
 		list_del_init(pos);
 		free(tmp);
 	}
-	return 0; /* 释放资源 */
+	return 0;
 }
